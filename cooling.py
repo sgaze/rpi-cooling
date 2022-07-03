c1a955dbc89bb7c647843f586e78a54dba48f4bd
Samuel Gazé <samuel.gaze@gmail.com>
2022-07-03 23:37:25 +0200

    Probing delay

--- cooling.py
+++ cooling.py
@@ -21,6 +21,7 @@
 
 
 from time import sleep
+from datetime import datetime
 from logging import Logger
 import RPi.GPIO as GPIO
 import Adafruit_DHT
@@ -46,51 +47,61 @@ def loop(logger: Logger, temp_low: float, temp_high: float, delay: int):
     GPIO.setup(RELAY_GAIN, GPIO.OUT)
     # GPIO.setwarnings(False)
 
-    logger.info('Min=%s  Max=%s', temp_low, temp_high)
+    logger.info('Starting: Min=%s  Max=%s', temp_low, temp_high)
 
     try:
+        last_probe_timestamp = None
+
         while True:
-            i = 0
 
-            while i < 5:
+            relay_state = GPIO.input(RELAY_GAIN)
 
-                relay_state = GPIO.input(RELAY_GAIN)
+            humidity, temperature = Adafruit_DHT.read_retry(
+                TEMP_SENSOR, TEMP_PROBE_PIN)
 
-                humidity, temperature = Adafruit_DHT.read_retry(
-                    TEMP_SENSOR, TEMP_PROBE_PIN)
+            if (
+                humidity is not None
+                and temperature is not None
+            ):
+                system_status = 'Cooling' if relay_state else 'Idle'
+                logger.info('Temp={0:0.1f}°C  Humidity={1:0.1f}% | {2}'.format(
+                    temperature,
+                    humidity,
+                    system_status
+                ))
 
-                if humidity is not None and temperature is not None:
-                    system_status = 'Cooling' if relay_state else 'Idle'
-                    logger.info('Temp={0:0.1f}°C  Humidity={1:0.1f}% | {2}'.format(
-                        temperature,
-                        humidity,
-                        system_status
-                    ))
+                # Switch cooling no more often than every 60s
+                refresh_switch = True
+                if last_probe_timestamp:
+                    probe_difference = (datetime.now() - last_probe_timestamp)
+                    refresh_switch = probe_difference.total_seconds() > 60
 
-                    if i == 0:
-                        logger.info('Checking cooling system: %s',
-                                    system_status)
+                if refresh_switch:
+                    logger.info('Probing: Min=%s  Max=%s | %s',
+                                temp_low, temp_high, system_status)
 
-                        if (
-                            temperature >= temp_high
-                            and not relay_state
-                        ):
-                            logger.info(
-                                'Starting cooling system, target=%s', temp_low)
-                            GPIO.output(RELAY_GAIN, GPIO.HIGH)
-                        elif (
-                            temperature < temp_low
-                            and relay_state
-                        ):
-                            logger.info(
-                                'Stopping cooling system, target=%s', temp_high)
-                            GPIO.output(RELAY_GAIN, GPIO.LOW)
+                    if (
+                        temperature >= temp_high
+                        and not relay_state
+                    ):
+                        logger.info(
+                            'Starting cooling, target=%s', temp_low)
+                        GPIO.output(RELAY_GAIN, GPIO.HIGH)
+                    elif (
+                        temperature < temp_low
+                        and relay_state
+                    ):
+                        logger.info(
+                            'Stopping cooling, target=%s', temp_high)
+                        GPIO.output(RELAY_GAIN, GPIO.LOW)
 
-                else:
-                    logger.warn('Failed to get reading. Try again!')
+                    # Last successful switch refresh
+                    last_probe_timestamp = datetime.now()
 
-                i += 1
-                sleep(delay)
+            else:
+                logger.warn('Failed to get reading. Try again!')
+
+            sleep(delay)
 
     except KeyboardInterrupt:
         pass
